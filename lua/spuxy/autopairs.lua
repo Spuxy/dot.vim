1bf623599a22985150705874e22484a20c3ecafe
Filip Boye-Kofi <filip.boye.kofi@livesport.eu>
2024-11-01 13:34:18 +0100

    autopairs ✅ refactored

--- lua/spuxy/autopairs.lua
+++ lua/spuxy/autopairs.lua
@@ -1,12 +1,10 @@
 local M = {
 	"windwp/nvim-autopairs",
-}
-
-M.config = function()
-	require("nvim-autopairs").setup({
+	opts = {
+		enable_check_bracket_line = false, -- Don't add pairs if it already has a close pair in the same line
 		check_ts = true,
 		disable_filetype = { "TelescopePrompt", "spectre_panel" },
-	})
-end
+	},
+}
 
 return M
